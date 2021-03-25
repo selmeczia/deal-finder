2ccc54c6b995e9091c1c9f2add553e4ac2142936
Adam <monk0909@gmail.com>
2021-03-26 00:10:14 +0100

    Added todos and input example

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("initial commit")
+# print("initial commit")
+
+
+# TODO: load inputs
+# TODO: iterate over input links
+# TODO: find prices of products
+# TODO: add extra values if available (avaliability, free shipping, discounted)
+# TODO: create database for the scraped data
+# TODO: remove unnecessary data (look for old project)
+# TODO: deal trigger
+# TODO: notification for deal trigger
+# TODO: automated run
