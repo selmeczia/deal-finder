1c3698e22db2a660a941feb997ce17ff51ca0a72
Adam <monk0909@gmail.com>
2021-04-05 14:53:32 +0200

    Added duplicate removal, output save

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from module import *
 
 
 config_path = "config/"
-product_group_input = config_path + "product_group_input.csv"
-chromedriver = "C:\Program Files (x86)\chromedriver.exe"
+output_path = "output/"
 
-DealFinder(product_group_input, chromedriver).task_scheduler()
+product_group_input = config_path + "product_group_input.csv"
+chromedriver_path = "C:\Program Files (x86)\chromedriver.exe"
+
+DealFinder(product_group_input, chromedriver_path, output_path).task_scheduler()
 
 #browser = webdriver.Chrome(chromedriver)
 #browser.get("https://ipon.hu/shop/csoport/szamitogep-alkatresz/videokartya?price=23790-299790&sortOrder=olcso&page=1")
--- module.py
+++ module.py
@@ -6,121 +6,120 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import numpy as np
+from datetime import datetime
+import os
 
 # TODO: load inputs
 
 
 class DealFinder:
 
-    def __init__(self, product_group_path, chromedriver):
+    def __init__(self, product_group_path, chromedriver_path, output_path):
         self.product_group_df = pd.DataFrame()
         self.product_group_path = product_group_path
-        self.chromedriver = chromedriver
+        self.chromedriver = chromedriver_path
+        self.output_path = output_path
 
     def task_scheduler(self):
         self.read_inputs()
-        self.iterate_over_lines()
+        self.scrape_handler()
 
     def read_inputs(self):
-        self.product_group_df = pd.read_csv(self.product_group_path, sep = ";")
-        return self.product_group_df
+        self.input_group_df = pd.read_csv(self.product_group_path, sep = ";")
 
-    def iterate_over_lines(self):
-        for index, row in self.product_group_df.iterrows():
-            name = row.tolist()[0] + ".csv"
-            link = row.tolist()[1]
+    def df_creation(self, product_dict, run_name):
+        df_name = run_name + "_df"
+        vars()[df_name] = pd.DataFrame.from_dict(product_dict, orient="index", columns=["Price", "Timestamp"]) \
+            .reset_index().rename(columns={"index": "Title"})
+        #self.current_df_name = vars()[df_name]
+        print("Dataframe created")
 
-            path = "./output/ " + name
+        return vars()[df_name]
+
+    def duplicate_removal(self, df):
+        path = self.output_path + self.current_run_type + ".csv"
+        df["Latest run"] = ""
+
+        if os.path.isfile(path):
+            df['Latest run'] = 1
+            imported = pd.read_csv(path, sep = ";")
+            imported_without_latest = imported[imported['Latest run'] == 0]
+            imported_only_latest = imported[imported['Latest run'] == 1].copy()
+            imported_only_latest['Latest run'] = 0
+            latest_and_current = imported_only_latest.append(df)
+            removed_duplicates = latest_and_current.drop_duplicates(subset=['Title', 'Price'], keep='last')
+
+            output = imported_without_latest.append(removed_duplicates)
+            df = output
+        else:
+            df["Latest run"] = 0
+
+        print("Duplicates removed")
+        return df
+
+    def save_df(self, df):
+
+        path = self.output_path + self.current_run_type + ".csv"
+        df.to_csv(path, index=False, header=True, sep=";")
+        print("Dataframe saved")
+
+    def scrape_handler(self):
+        for index, row in self.input_group_df.iterrows():
+            current_store = row[0].split("_")[0]
+            self.current_run_type = row[0]
+            current_link = row[1]
+
+            if current_store == "Ipon":
+                current_scrape = self.scrape_Ipon_group(current_link)
+                current_df_raw = self.df_creation(current_scrape, current_store)
+                current_df_processed = self.duplicate_removal(current_df_raw)
+                self.save_df(current_df_processed)
 
 
-            browser = webdriver.Chrome(self.chromedriver)
-            browser.get(link)
+    def scrape_Ipon_group(self, link):
 
-            while True:
-                next_page_btn = browser.find_elements_by_class_name("product-list__show-more-button")
-                time.sleep(1)
-                if len(next_page_btn) < 1:
-                    print("All pages loaded")
-                    break
-                else:
-                    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "product-list__show-more-button"))).click()
+        browser = webdriver.Chrome(self.chromedriver)
+        browser.get(link)
 
-            cards = browser.find_elements_by_class_name("shop-card__body")
+        while True:
+            next_page_btn = browser.find_elements_by_class_name("product-list__show-more-button")
+            time.sleep(2)
+            if len(next_page_btn) < 1:
+                print("All pages loaded")
+                break
+            else:
+                WebDriverWait(browser, 10)\
+                    .until(EC.element_to_be_clickable((By.CLASS_NAME, "product-list__show-more-button"))).click()
 
-            title_list = []
-            price_list = []
+        cards = browser.find_elements_by_class_name("shop-card__body")
 
-            for card in cards:
-                title_element = card.find_elements_by_class_name("shop-card__title")[0].text
-                price_element = card.find_elements_by_class_name("shop-card__price-block")[0].text
+        product_dict = {}
+        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M")
 
-                if (price_element.find("\n") != -1):
-                    price_element = price_element.split("\n")[1]
-                    price_int =  int(price_element.split(' Ft')[0].replace(' ', ''))
-                elif (price_element == "csak b2b"):
-                    price_int = np.nan
-                else:
-                    price_int = int(price_element.split(' Ft')[0].replace(' ', ''))
+        for card in cards:
+            title_element = card.find_elements_by_class_name("shop-card__title")[0].text
+            price_element = card.find_elements_by_class_name("shop-card__price-block")[0].text
 
-                title_list.append(title_element)
-                price_list.append(price_int)
+            if price_element.find("\n") != -1:
+                price_element = price_element.split("\n")[1]
+                price_int =  int(price_element.split(' Ft')[0].replace(' ', ''))
+                product_dict[title_element] = [price_int, timestamp]
 
-            df = pd.DataFrame(list(zip(title_list, price_list)), columns=['Title', "Price"])
+            elif price_element == "csak b2b":
+                continue
+            elif price_element == "":
+                continue
+            else:
+                price_int = int(price_element.split(' Ft')[0].replace(' ', ''))
+                product_dict[title_element] = [price_int, timestamp]
 
-            print(df)
+        print("Scraping done")
+        return product_dict
 
 
 
-            # print(source)
 
-            # print(soup.find_all('div', class_='shop-card__body'))
-
-            # for item in soup.find_all('div', class_='shop-card__price'):
-            #
-            #     item_price_raw = item.find('div', class_='row-price').text
-            #     item_price = item_price_raw.split(' Ft')[0].replace(' ', '')
-            #
-            #     store_name = item.find('div', class_='shopname').text
-            #
-            #     link = item.find('a', class_='jumplink-overlay initial')
-            #     item_link = link['href']
-            #
-            #     price_list.append(int(item_price))
-            #     store_list.append(store_name)
-            #     link_list.append(item_link)
-            #
-            # if not (price_list):
-            #     pass
-            # else:
-            #     min_price = min(price_list)
-            #     df = pd.DataFrame(list(zip(price_list, store_list, link_list)), columns=['Price', "Store", "Link"])
-            #     df["Min price"] = min_price
-            #     df["Date"] = datetime.now().strftime("%d/%m/%Y %H:%M")
-            #     df = df[["Date", "Price", "Min price", "Store", "Link"]]
-            #
-            #     df['Latest_run'] = ""
-            #
-            #     # if not first run:
-            #     if os.path.isfile(path):
-            #         df['Latest_run'] = 1
-            #         imported = pd.read_csv(path)
-            #         imported_without_latest = imported[imported['Latest_run'] == 0]
-            #         imported_only_latest = imported[imported['Latest_run'] == 1].copy()
-            #         # https://www.dataquest.io/blog/settingwithcopywarning/
-            #         imported_only_latest['Latest_run'] = 0
-            #         latest_and_current = imported_only_latest.append(df)
-            #         removed_duplicates = latest_and_current.drop_duplicates(subset=['Store', 'Price'], keep='last')
-            #
-            #         output = imported_without_latest.append(removed_duplicates)
-            #         df = output
-            #
-            #     else:
-            #         df['Latest_run'] = 0
-
-# TODO: find prices of products
 # TODO: add extra values if available (avaliability, free shipping, discounted)
-# TODO: create database for the scraped data
-# TODO: remove unnecessary data (look for old project)
 # TODO: deal trigger
 # TODO: notification for deal trigger
 # TODO: automated run
